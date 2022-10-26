746ce8f95e6d49a94801849ed16d0338e5f6ef56
jamrockshuffle <70891061+jamrockshuffle@users.noreply.github.com>
2022-10-26 11:06:06 +0300

    Cors

---
+++ src/main/java/com/websecurity/store/security/configuration/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.websecurity.store.security.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class CorsConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/*") // /**
+                .allowedOrigins("http://localhost:4444/")
+                .allowedMethods("GET", "POST")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
